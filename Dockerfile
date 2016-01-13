5418c16a656c573338688d05451dffa0ae182033
Nicholas Digati <nicholas@factual.com>
2016-01-12 17:16:01 -0800

    Changed S3 backing to use Goofys

--- Dockerfile
+++ Dockerfile
@@ -1,40 +1,21 @@
-FROM node
+FROM golang:latest
 MAINTAINER Nicholas Digati <nicholas@factual.com>
 
-RUN apt-get -y update && apt-get -y install \
- automake \
- autotools-dev \
- build-essential \
- g++ \
- git \
- libcurl4-gnutls-dev \
- libfuse-dev \
- libssl-dev \
- libxml2-dev \
- make \
- pkg-config \
- && rm -rf /var/lib/apt/lists/*
+RUN apt-get -y update
+RUN curl -sL https://deb.nodesource.com/setup_4.x | bash -
+RUN apt-get -y install fuse \
+  build-essential \
+  nodejs
 
-RUN git clone https://github.com/s3fs-fuse/s3fs-fuse.git && \
-  cd s3fs-fuse && \
-  ./autogen.sh && \
-  ./configure && \
-  make && \
-  make install
+RUN go get github.com/kahing/goofys
+RUN go install github.com/kahing/goofys
 
-RUN mkdir -p /opt/sinopia/bucket
-
-WORKDIR /opt/sinopia
+RUN mkdir -p bucket/ ~/.aws/
 
 RUN npm install js-yaml sinopia
 
-COPY start.sh /opt/sinopia/start.sh
-
-# Only need this if not pulling config from outside source(start.sh)
-# COPY config.yaml /opt/sinopia/config.yaml
-
-CMD [ "/opt/sinopia/start.sh" ]
+COPY start.sh .
 
 EXPOSE 4873
 
-VOLUME /opt/sinopia/bucket
+CMD [ "./start.sh" ]
--- start.sh
+++ start.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+if [ -z $SINOPIA_CONFIG ]; then
+  echo "You need to set SINOPIA_CONFIG"
+  exit 1
+fi
+
+# Get config file
+# If you want to pull the config from somewhere else
+# Otherwise comment this out the curl command
+curl -s $SINOPIA_CONFIG > config.yaml
+
+echo "Received sinopia config:"
+cat config.yaml
+
+echo "Current user:"
+whoami
+
+# echo "Base directory:"
+# ls -lrt bucket/storage
+
+echo "Write to storage:"
+touch bucket/storage/test
+
+# Set file and folder permissions correctly
+# Only needed if you upload files/folders directly into S3 bucket
+# Otherwise permissions are set correctly
+# Only need to run these if not using an empty S3 bucket
+# find bucket/storage -type d -exec chmod 755 {} \;
+# find bucket/storage -type f -exec chmod 644 {} \;
+
+node node_modules/sinopia/bin/sinopia --config config.yaml &
+
 # Abort if the SINOPIA_BUCKET was not provided
 if [ -z $SINOPIA_BUCKET ]; then
   echo "You need to set BUCKET environment variable. Aborting!"
@@ -26,37 +57,7 @@ if [ ! -z $AWS_ACCESS_KEY_ID ] && [ ! -z $AWS_SECRET_ACCESS_KEY ]; then
 fi
 
 # start s3 fuse
-/usr/local/bin/s3fs $SINOPIA_BUCKET /opt/sinopia/bucket/ -o allow_other -o nonempty -o mp_umask="0022" #-d -d -f -o f2 -o curldbg
+# /usr/local/bin/s3fs $SINOPIA_BUCKET /opt/sinopia/bucket/ -o allow_other -o nonempty -o mp_umask="0022" #-d -d -f -o f2 -o curldbg
 
-
-if [ -z $SINOPIA_CONFIG ]; then
-  echo "You need to set SINOPIA_CONFIG"
-  exit 1
-fi
-
-# Get config file
-# If you want to pull the config from somewhere else
-# Otherwise comment this out the curl command
-mkdir -p /opt/sinopia
-curl -s $SINOPIA_CONFIG > /opt/sinopia/config.yaml
-
-echo "Received sinopia config:"
-cat /opt/sinopia/config.yaml
-
-echo "Current user:"
-whoami
-
-# echo "Base directory:"
-# ls -lrt /opt/sinopia/bucket/storage
-
-echo "Write to storage:"
-touch /opt/sinopia/bucket/storage/test
-
-# Set file and folder permissions correctly
-# Only needed if you upload files/folders directly into S3 bucket
-# Otherwise permissions are set correctly
-# Only need to run these if not using an empty S3 bucket
-# find /opt/sinopia/bucket/storage -type d -exec chmod 755 {} \;
-# find /opt/sinopia/bucket/storage -type f -exec chmod 644 {} \;
-
-node /opt/sinopia/node_modules/sinopia/bin/sinopia --config /opt/sinopia/config.yaml
+# start goofys
+$GOPATH/bin/goofys -f -o allow_other -o nonempty $SINOPIA_BUCKET bucket
